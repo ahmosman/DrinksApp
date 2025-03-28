b3c9992936acff4a822f9e6c172917a21548a18b
ahmosman <ahmedosman.inf@gmail.com>
2025-03-28 22:55:17 +0100

    init commit

---
+++ app/src/main/java/com/example/drinksapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.drinksapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import com.example.drinksapp.ui.theme.DrinksAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DrinksAppTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    Text(text = "Hello, World!")
+                }
+            }
+        }
+    }
+}
