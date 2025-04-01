3755a79aa003c95728a4cfe838d60eaf7403ca07
ahmosman <ahmedosman.inf@gmail.com>
2025-04-01 09:12:56 +0300

    DB Fix copyDatabase

--- app/src/main/java/com/example/drinksapp/DatabaseHelper.kt
+++ app/src/main/java/com/example/drinksapp/DatabaseHelper.kt
@@ -3,6 +3,8 @@ package com.example.drinksapp
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import java.io.FileOutputStream
+import java.io.InputStream
 
 class DatabaseHelper(context: Context) :
     SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
@@ -10,7 +12,29 @@ class DatabaseHelper(context: Context) :
     private val cocktails = mutableListOf<Cocktail>()
 
     init {
+        val dbFile = context.getDatabasePath(DATABASE_NAME)
+        if (!dbFile.exists()) {
+            copyDatabase(context)
+        }
         loadCocktails()
+    }
+
+    private fun copyDatabase(context: Context) {
+        val dbPath = context.getDatabasePath(DATABASE_NAME)
+        dbPath.parentFile?.mkdirs()
+
+        val inputStream: InputStream = context.assets.open(DATABASE_NAME)
+        val outputStream = FileOutputStream(dbPath)
+
+        val buffer = ByteArray(1024)
+        var length: Int
+        while (inputStream.read(buffer).also { length = it } > 0) {
+            outputStream.write(buffer, 0, length)
+        }
+
+        outputStream.flush()
+        outputStream.close()
+        inputStream.close()
     }
 
     private fun loadCocktails() {
